13f844363920ee66788c221fc05add47c82e1c4f
Jonathan Warner <jewarner57@gmail.com>
2020-11-29 16:30:52 -0500

    finished view plan template

--- plan/routes.py
+++ plan/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
+import requests
+
 
 plan = Blueprint("plan", __name__, template_folder="templates")
 
@@ -8,9 +10,15 @@ def createPlan():
     if request.method == "GET":
         return render_template("createPlan.html")
     else:
-        return "post recieved"
+        return redirect(url_for("plan.viewPlan"))
 
 
 @plan.route("/viewPlan")
 def viewPlan():
+
+    # url = "http://api.openweathermap.org/data/2.5/weather"
+    # params = {"appid": API_KEY, "q": city, "units": units}
+
+    # result_json = requests.get(url, params=params).json()
+
     return render_template("viewPlan.html")
